2f4aacbe56ffef00d7f30871e8bd258c970f1993
VishalreddyA <37933100+VishalreddyA@users.noreply.github.com>
2018-03-30 19:21:06 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+package com.sampleEx;
+
+public class Hello 
+{
+
+	
+	public static void main(String[] args)
+	{
+		int a=10;
+		a++;
+	System.out.println("the resut is:"+a);
+	
+		
+		
+
+	}
+
+}
